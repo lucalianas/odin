59f1917d13603f0f19749654302240d9f9aee4a8
Luca Lianas <luca.lianas@crs4.it>
2017-10-03 15:40:19 +0200

    get detailed scoring for all cases (primary and secondary gleason score for all cores)

--- odin/get_cases_scoring.py
+++ odin/get_cases_scoring.py
@@ -41,19 +41,69 @@ class CasesOverallScoring(ProMortTool):
         self._logout()
 
 
-help_doc = """
+cos_help_doc = """
 add doc
 """
 
 
-def make_parser(parser):
-    parser.add_argument('--output-file', type=str, required=True, help='output file')
-
-
-def implementation(host, user, passwd, logger, args):
+def cos_implementation(host, user, passwd, logger, args):
     case_scoring = CasesOverallScoring(host, user, passwd, logger)
     case_scoring.run(args.output_file)
 
 
+# -------------------------------------------------------------
+class DetailedCaseOverallScoring(CasesOverallScoring):
+
+    def __init__(self, host, user, passwd, logger):
+        super(DetailedCaseOverallScoring, self).__init__(host, user, passwd, logger)
+
+    def _get_detailed_score(self, case_id):
+        url = urljoin(self.promort_host, 'api/odin/reviews/%s/score/details/' % case_id)
+        response = self.promort_client.get(url)
+        if response.status_code == requests.codes.OK:
+            return response.json().values()
+        return []
+
+    def run(self, out_file):
+        self._login()
+        perm_ok = self._check_odin_permissions()
+        if perm_ok:
+            cases = self._get_cases()
+            with open(out_file, 'w') as output_file:
+                writer = csv.DictWriter(output_file,
+                                        ['case', 'laboratory', 'slide', 'core', 'primary_gleason', 'secondary_gleason'])
+                writer.writeheader()
+                for case, lab in cases:
+                    score_details = self._get_detailed_score(case)
+                    for review_details in score_details:
+                        for slide, cores in review_details['slides_details'].iteritems():
+                            for core in cores:
+                                writer.writerow({
+                                    'case': case,
+                                    'laboratory': lab,
+                                    'slide': slide,
+                                    'core': core['core_label'],
+                                    'primary_gleason': core['primary_gleason_score'],
+                                    'secondary_gleason': core['secondary_gleason_score']
+                                })
+        self._logout()
+
+
+dcos_help_doc = """
+add doc
+"""
+
+
+def dcos_implementation(host, user, passwd, logger, args):
+    detailed_case_scoring = DetailedCaseOverallScoring(host, user, passwd, logger)
+    detailed_case_scoring.run(args.output_file)
+
+
+# -------------------------------------------------------------
+def make_parser(parser):
+    parser.add_argument('--output-file', type=str, required=True, help='output file')
+
+
 def register(registration_list):
-    registration_list.append(('cases_overall_scoring', help_doc, make_parser, implementation))
+    registration_list.append(('cases_overall_scoring', cos_help_doc, make_parser, cos_implementation))
+    registration_list.append(('detailed_cases_scoring', dcos_help_doc, make_parser, dcos_implementation))
